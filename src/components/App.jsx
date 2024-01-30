444947547d0eda5d911b61820b1e827d2e9e99d5
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-01-30 16:17:14 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { PokemonForm } from './PokemonForm';
 import { PokemonInfo } from './PokemonInfo';
 
-export class App extends Component {
-  state = {
-    pokemonName: '',
+export default function App() {
+  const [pokemonName, setPokemonName] = useState('');
+
+  const handleFormSubmit = pokemonName => {
+    setPokemonName(pokemonName);
   };
 
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
-  };
-
-  render() {
-    return (
-      <>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
-      </>
-    );
-  }
+  return (
+    <>
+      <PokemonForm onSubmit={handleFormSubmit} />
+      <PokemonInfo pokemonName={pokemonName} />
+    </>
+  );
 }
